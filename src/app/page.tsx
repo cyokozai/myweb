0e9f634b2079fff1661b1b8f4068f6ff421ce323
cyokozai <111193076+cyokozai@users.noreply.github.com>
2024-02-24 11:50:21 +0900

    20240224

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,22 @@ import { url } from "inspector";
 import Image from "next/image";
 import { list } from "postcss";
 import { title } from "process";
+// import React, { useState } from 'react';
+import { Link as Scroll } from 'react-scroll';
+
 
 interface PostProps {
   title: string;
   url: string;
 }
+
+
+// function scroll( y: number, duration: number, offset: number) {
+//   return(
+//     <Scroll to="concept" smooth={true} duration={duration} offset={offset}></Scroll>
+//   )
+// }
+
 
 function Posts({ title, url }: PostProps){
   return (
@@ -18,9 +29,15 @@ function Posts({ title, url }: PostProps){
   );
 }
 
+
 export default function Home() {
   const props1 = {
-    title: "example",
+    title: "example1",
+    url: "https://example.com"
+  };
+
+  const props2 = {
+    title: "example2",
     url: "https://example.com"
   };
 
@@ -28,6 +45,10 @@ export default function Home() {
     <div>
       <h1>Hello World</h1>
       <Posts title = {props1.title} url = {props1.url} />
+      <Posts title = {props2.title} url = {props2.url} />
+      <br>
+      </br>
+      <section id="concept"></section>
     </div>
   );
 }
