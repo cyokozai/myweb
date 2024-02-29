587fa2ec58118906c7fb20ddef835094aad867f9
cyokozai <yuu4suke27@outlook.jp>
2024-02-29 16:00:56 +0900

    20240229

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,24 @@ import { url } from "inspector";
 import Image from "next/image";
 import { list } from "postcss";
 import { title } from "process";
+import "../src/app/index.css";
 
 interface PostProps {
   title: string;
   url: string;
 }
 
+
 function Posts({ title, url }: PostProps){
-  return (
+  return(
     <article>
-      <a href={url}>
+      <a href={url} className="bg-red-500 text-white font-bold py-2 px-4">
         <h3>{title}</h3>
       </a>
     </article>
   );
 }
+
 
 export default function Home() {
   const props1 = {
@@ -24,7 +27,7 @@ export default function Home() {
     url: "https://example.com"
   };
 
-  return (
+  return(
     <div>
       <h1>Hello World</h1>
       <Posts title = {props1.title} url = {props1.url} />
---
+++ src/app/whoami/page.tsx
@@ -0,0 +1,19 @@
+import { url } from "inspector";
+import Image from "next/image";
+import { list } from "postcss";
+import { title } from "process";
+
+
+interface PostProps {
+  title: string;
+  url: string;
+}
+
+
+export default function Whoami() {
+    return(
+        <div>
+            
+        </div>
+    )
+}
