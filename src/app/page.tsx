e5add4b5f07603c765309a38d37ef2ea38184821
cyokozai <yuu4suke27@outlook.jp>
2024-02-29 18:21:39 +0900

    20240229

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,18 @@ import { url } from "inspector";
 import Image from "next/image";
 import { list } from "postcss";
 import { title } from "process";
-import "../src/app/index.css";
 
 
 interface PostProps {
   title: string;
   url: string;
+}
+
+
+interface MenuProps {
+  a1: string;
+  a2: string;
+  a3: string;
 }
 
 
@@ -22,16 +28,42 @@ function Posts({ title, url }: PostProps){
 }
 
 
+function Menu({ a1, a2, a3 }: MenuProps){
+  return(
+    <nav>
+      <ul>
+        <li>
+          <a href="#">a1</a>
+        </li>
+        <li>
+          <a href="#">a2</a>
+        </li>
+        <li>
+          <a href="#">a3</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+
 export default function Home() {
   const props1 = {
     title: "example",
     url: "https://example.com"
   };
 
+  const menu1 = {
+    a1: "whoami",
+    a2: "About",
+    a3: "Contact"
+  };
+
   return(
     <div>
       <h1>Hello World</h1>
       <Posts title = {props1.title} url = {props1.url} />
+      <Menu a1 = {menu1.a1} a2 = {menu1.a2} a3 = {menu1.a3} />
     </div>
   );
 }
